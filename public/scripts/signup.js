4c606f0dfefa9749c013099ad340dedf15ce04de
Logan Zehr <zehrl315@outlook.com>
2021-01-29 12:10:38 -0800

    handlebars editing

--- public/scripts/signup.js
+++ public/scripts/signup.js
@@ -1,57 +1,19 @@
-console.log("signup.js")
+console.log("loading signup.js")
 $(function () {
 
   // User submits search query
-  $("#search-result").on("submit", function (event) {
+  $("#searchButton").on("click", function (event) {
     event.preventDefault();
 
     console.log("Starting query.")
-
-    // const recipeSearch = {
-    //   name: $("#search-result")
-    //     .val()
-    //     .trim(),
-    //   ingredient: $("#ingredients").val(),
-    //   instruction: $("#instructions"),
-    // };
 
     const recipeSearch = $("#search-result").val().trim()
 
     const query = `?ingredient=${recipeSearch}`
 
-    $.ajax("/search" + query, {
-      type: "GET"
-      // data: newRecipe,
-    }).then(function () {
-      console.log("Searching...");
-      // location.reload();
-    });
+    // redirect to /search route with query text
+    window.location.href = "/search" + query;
+
   });
 
-  // $("#updateButton").on("click", function (event) {
-  //   event.preventDefault();
-
-  //   var id = $(this).data("id");
-
-  //   $.ajax("/api/recipes/", {
-  //     type: "PUT",
-  //     data: id
-  //   }).then(function () {
-  //     console.log("recipe has been updated");
-  //     location.reload();
-  //   });
-  // });
-
-  // $("#deleteButton").on("click", function (event) {
-  //   event.preventDefault();
-
-  //   var id = $(this).data("id");
-
-  //   $.ajax("/api/recipes/" + id, {
-  //     type: "DELETE",
-  //   }).then(function () {
-  //     console.log("recipe has been deleted");
-  //     location.reload();
-  //   });
-  // });
 });
