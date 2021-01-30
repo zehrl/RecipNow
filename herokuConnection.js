6b3858389121a186e22746290194896a95dbc64d
Logan Zehr <zehrl315@outlook.com>
2021-01-29 18:19:36 -0800

    weird commit issues

--- herokuConnection.js
+++ herokuConnection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "password",
+        password: process.env.DB_PASS,
         database: "recipnowDB"
     });
 };
